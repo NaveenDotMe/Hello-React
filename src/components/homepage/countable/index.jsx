adea81cb32192488f12b2b19169495c85c73497d
Brock-99 <elmatadordelmar99@gmail.com>
2022-07-21 10:16:24 +0530

    State Handling

--- src/components/homepage/countable/index.jsx
+++ src/components/homepage/countable/index.jsx
@@ -5,18 +5,42 @@ import Button from "@mui/material/Button";
 class Countable extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    increaseCount() {
+        console.log("Increase Function Calling")
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    decreaseCount() {
+        console.log("Decrease Function Calling")
+        this.setState({
+            count: this.state.count - 1
+        })
     }
 
     render() {
         return (
             <div>
                 <Typography variant="h4" component="div" gutterBottom>
-                    Count : 0
+                    Count : {this.state.count}
                 </Typography>
                 <Button variant="contained"
-                        onClick={() =>{
+                        onClick={() => {
                             console.log("Clicked")
+                            this.increaseCount()
                         }}>Increase</Button>
+
+                <Button variant="contained"
+                        onClick={() => {
+                            console.log("Clicked")
+                            this.decreaseCount()
+                        }}>Decrease</Button>
             </div>
         )
     }
