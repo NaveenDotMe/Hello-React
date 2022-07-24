6f975a05e0f3f4f17abc2650c420303ad881fc55
Brock-99 <elmatadordelmar99@gmail.com>
2022-07-24 22:22:00 +0530

    Aded Grid Layout

--- src/app/App.js
+++ src/app/App.js
@@ -4,14 +4,16 @@ import {Route, Routes} from "react-router-dom";
 import Login from "../pages/session/login";
 import NotFound from "../pages/session/NotFound";
 import FlexBox from "../pages/Layout/flexBox";
+import GridLayout from "../pages/Layout/Grid";
 
 function App() {
     return (
         <Routes>
             <Route excat path="/" element={<HomePage/>}/>
             <Route path="login" element={<Login/>}/>
-            <Route path="*" element={<NotFound/>}/>
             <Route path="flex" element={<FlexBox/>}/>
+            <Route path="grid" element={<GridLayout/>}/>
+            <Route path="*" element={<NotFound/>}/>
         </Routes>
     );
 }
