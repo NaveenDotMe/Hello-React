b6ddde7712e3ed745bf7e98d431272866153e56b
Brock-99 <elmatadordelmar99@gmail.com>
2022-07-25 10:41:00 +0530

    Create and instance for axios

---
+++ src/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+//base URL
+const instance = axios.create({
+    //baseURL
+    baseURL:"https://jsonplaceholder.typicode.com/"
+    //Header
+    //Timeout
+
+})
+export default instance
