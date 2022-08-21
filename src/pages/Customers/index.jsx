caff48c2ad8502e90b93d97b603a6f0ee5c92627
Brock-99 <elmatadordelmar99@gmail.com>
2022-08-21 05:37:42 +0530

    Customer Screen added onChange and onClick

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -1,17 +1,30 @@
 import React, {Component} from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+import {TextValidator, ValidatorForm} from "react-material-ui-form-validator";
 import GDSEButton from "../../components/common/button";
 
-class Customers extends Component{
+class Customers extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            formData: {
+                id: '',
+                name: '',
+                address: '',
+                salary: 0
+            }
+        }
+    }
+
+    submitCustomer = (e) => {
+        let formData = this.state.formData;
+        console.log("FormData " + JSON.stringify(formData))
     }
 
     render() {
-        return(
-            <ValidatorForm ref="form" className="pt-2" onSubmit={this.submitCustomer} >
+        return (
+            <ValidatorForm ref="form" className="pt-2" onSubmit={this.submitCustomer}>
                 <Grid container className="pt-2" spacing={3}>
                     <Grid item lg={12} xs={12} sm={12} md={12}>
                         <Typography variant="h2">Customer Manage</Typography>
@@ -23,13 +36,13 @@ class Customers extends Component{
                             placeholder="Customer Id"
                             variant="outlined"
                             size="small"
-                            style={{ width: '100%' }}
-                            // value={}
-                            // onChange={(e) => {
-                            //     let formData = this.state.formData
-                            //     formData.id = e.target.value
-                            //     this.setState({ formData })
-                            // }}
+                            style={{width: '100%'}}
+                            value={this.state.formData.id}
+                            onChange={(e) => {
+                                let formData = this.state.formData
+                                formData.id = e.target.value
+                                this.setState({formData})
+                            }}
                             validators={['required']}
                         />
                     </Grid>
@@ -40,13 +53,13 @@ class Customers extends Component{
                             placeholder="Customer Name"
                             variant="outlined"
                             size="small"
-                            style={{ width: '100%' }}
-                            // value={}
-                            // onChange={(e) => {
-                            //     let formData = this.state.formData
-                            //     formData.id = e.target.value
-                            //     this.setState({ formData })
-                            // }}
+                            style={{width: '100%'}}
+                            value={this.state.formData.name}
+                            onChange={(e) => {
+                                let formData = this.state.formData
+                                formData.name = e.target.value
+                                this.setState({formData})
+                            }}
                             validators={['required']}
                         />
                     </Grid>
@@ -57,13 +70,13 @@ class Customers extends Component{
                             placeholder="Customer Address"
                             variant="outlined"
                             size="small"
-                            style={{ width: '100%' }}
-                            // value={}
-                            // onChange={(e) => {
-                            //     let formData = this.state.formData
-                            //     formData.id = e.target.value
-                            //     this.setState({ formData })
-                            // }}
+                            style={{width: '100%'}}
+                            value={this.state.formData.address}
+                            onChange={(e) => {
+                                let formData = this.state.formData
+                                formData.address = e.target.value
+                                this.setState({formData})
+                            }}
                             validators={['required']}
                         />
                     </Grid>
@@ -74,17 +87,18 @@ class Customers extends Component{
                             placeholder="Customer Salary"
                             variant="outlined"
                             size="small"
-                            style={{ width: '100%' }}
-                            // value={}
-                            // onChange={(e) => {
-                            //     let formData = this.state.formData
-                            //     formData.id = e.target.value
-                            //     this.setState({ formData })
-                            // }}
+                            style={{width: '100%'}}
+                            value={this.state.formData.salary}
+                            onChange={(e) => {
+                                let formData = this.state.formData
+                                formData.salary = e.target.value
+                                this.setState({formData})
+                            }}
                             validators={['required']}
                         />
                     </Grid>
-                    <Grid container style={{marginTop: '10px'}} direction="row" justifyContent="flex-end" alignItems="center">
+                    <Grid container style={{marginTop: '10px'}} direction="row" justifyContent="flex-end"
+                          alignItems="center">
                         <GDSEButton label="save" type="submit" size="small" color="primary" variant="contained"/>
                     </Grid>
                 </Grid>
@@ -92,4 +106,5 @@ class Customers extends Component{
         );
     }
 }
+
 export default Customers;
